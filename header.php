2dfa25a334b2d69d482a887ae47c158c85c6e218
GD <guillaume.duran@garmin.com>
2020-05-27 18:44:53 +0200

    Adds favicon

--- header.php
+++ header.php
@@ -16,6 +16,10 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() ?>/img/favicon_io/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() ?>/img/favicon_io/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() ?>/img/favicon_io/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
 
 	<?php wp_head(); ?>
 </head>
